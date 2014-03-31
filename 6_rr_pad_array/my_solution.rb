76da2872d3254ef16252a5e349ab6811c92acd20
Nztzlie <nataliebaer@Natalies-MacBook-Pro.local>
2014-03-31 10:42:52 -0400

    Exercise 6 updates

--- 6_rr_pad_array/my_solution.rb
+++ 6_rr_pad_array/my_solution.rb
@@ -24,8 +24,21 @@ end
 
 # 2. Second Person's solution I liked
 #    What I learned from this solution
+#  This was a completely different way of coding this solution, using until
+#  rather than if else.  It was also very efficient.
 # Copy solution here:
-
+class Array
+    def pad!(pad_length, pad_value = nil)
+        self << pad_value until self.length >= pad_length
+        self
+    end
+     
+    def pad(pad_length, pad_value = nil)
+        new_array = self.clone 
+        new_array << pad_value until new_array.length >= pad_length
+        new_array 
+    end
+end 
 
 
 
@@ -62,8 +75,29 @@ end
 
 
 # 4. My refactored solution:
-
+class Array
+    def pad!(new_length, new_element= nil)
+        if new_length <= self.length
+            return self
+        else
+            difference = new_length-self.length
+            while difference > 0
+                self << new_element
+                difference--
+            end    
+        end
+    end
+    
+    def pad(new_length, new_element = nil)
+        new_array = self.clone
+        new_array.pad!(new_length,new_element)
+    end    
+end
 
 
 
 # 5. Reflection
+# It is interesting to see how other people think through
+# coding logic. As I am new to coding logic and often am unsure
+# of myself, it helps to look at how other people have solved
+# the same problem.
